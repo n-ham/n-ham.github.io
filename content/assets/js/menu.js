60a1c027d11dd7b33f5bcb2598f55ab76b362d5f
n-ham <nicholas.charles.ham@gmail.com>
2022-11-04 22:38:12 +1100

    updated minification

--- content/assets/js/menu.js
+++ content/assets/js/menu.js
@@ -1,38 +1,32 @@
 const menu    = document.getElementById("menu-page");
 const content = document.getElementById("content");
 
-function close_menu()
-{
+function close_menu() {
   menu.style.visibility    = "hidden";
   content.style.visibility = "visible";
   content.style.display    = "block";
 }
 
-function show_menu()
-{
+function show_menu() {
   menu.style.visibility    = "visible";
   content.style.visibility = "hidden";
   content.style.display    = "none";
 }
 
-function toggle_submenu(i) 
-{
-  if(!window.event.ctrlKey)
-  {
+function toggle_submenu(i) {
+  if(!window.event.ctrlKey) {
     item = document.getElementsByClassName('has-submenu')[i];
     item_link = document.getElementsByClassName('submenu-link')[i];
     item_link_icon = document.getElementsByClassName('submenu-link-icon')[i];
     submenu = document.getElementsByClassName('submenu')[i];
-    if(item.classList.contains("submenu-active")) 
-    {
+    if(item.classList.contains("submenu-active")) {
       item.classList.remove("submenu-active");
       item_link.classList.remove("submenu-link-active");
       item_link_icon.classList.remove("fa-angle-up-png");
       item_link_icon.classList.add("fa-angle-down-png");
       submenu.style.display = "none";
     } 
-    else
-    {
+    else {
       item.classList.add("submenu-active");
       item_link.classList.add("submenu-link-active");
       item_link_icon.classList.add("fa-angle-up-png");
--- post-build.f
+++ post-build.f
@@ -1,8 +1,8 @@
 chmod u+w output/assets/js/bundle.js
-minify --dir output/assets/js/ output/assets/js/bundle.js
+minify --dir output/assets/js/ --js output/assets/js/bundle.js
 
 chmod u+w output/assets/css/style.css
-minify --dir output/assets/css/ output/assets/css/style.css
+minify --dir output/assets/css/ --css output/assets/css/style.css
 
 chmod u+w output/*.html
-minify --dir output/ output/*.html
+minify --dir output/ --html output/*.html
