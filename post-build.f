96014aa2f30738306c713877d6ef31c318317e91
n-ham <nicholas.charles.ham@gmail.com>
2022-11-17 00:49:20 +1100

    fixed up minification

--- post-build.f
+++ post-build.f
@@ -1,8 +1,8 @@
 chmod u+w output/assets/js/bundle.js
-minify --dir output/assets/js/ --js output/assets/js/bundle.js
+mantis-minify --dir output/assets/js/ --js output/assets/js/bundle.js
 
 chmod u+w output/assets/css/style.css
-minify --dir output/assets/css/ --css output/assets/css/style.css
+mantis-minify --dir output/assets/css/ --css output/assets/css/style.css
 
 chmod u+w output/*.html
-minify --dir output/ --html output/*.html
+mantis-minify --dir output/ --html output/*.html
